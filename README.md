bb62f0e647b8a805ede4d456ff8698757385a41d
AnandSundar <anandsundar96@gmail.com>
2017-12-12 12:13:21 -0500

    implemeted movie details page. Everything works fine

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
+<h1>MovieDBase</h1>
 
-
+<p>
+This is a hybrid application that uses OMDB API to get movie information using jQuery Mobile.
+</p>
 
 <h2>Technologies Used: </h2>
 <ul>
 <li>live-server package</li>
 <li>Node.js</li>
 <li>OMDB API</li>
-
 </ul>
--- js/main.js
+++ js/main.js
@@ -6,8 +6,19 @@ $(document).ready(function() {
   });
 });
 
+//runs before movie details page
+$(document).on('pagecontainerbeforeshow', function (event, ui) {
+  //check to see which page we are navigating to using the id
+  if(ui.toPage[0].id == 'movie'){
+    let movieId = sessionStorage.getItem('movieId');
+    getMovie(movieId);
+  }
+
+});
+
 //single movie selected. get the movie details
 function movieClicked(id) {
+  //store the imdb id in browser storage
   sessionStorage.setItem('movieId', id);
   $.mobile.changePage('movie.html');
 }
@@ -32,5 +43,36 @@ function getMovies(searchText) {
       `;
     });
     $('#movies').html(output).listview('refresh');
+  });
+}
+
+//get single movie
+function getMovie(movieId) {
+  $.ajax({
+    method: 'GET',
+    url: 'http://www.omdbapi.com?apikey=57330b36&i='+movieId
+  }).done(function(movie){
+    //console.log(movie);
+    let movieTop = `
+      <div style="text-align:center">
+        <h1>${movie.Title}</h1>
+        <img src="${movie.Poster}">
+      </div>
+    `;
+    $('#movieTop').html(movieTop);
+
+    let movieDetails = `
+      <li><strong>Genre:</strong> ${movie.Genre}</li>
+      <li><strong>Rated:</strong> ${movie.Rated}</li>
+      <li><strong>Released:</strong> ${movie.Released}</li>
+      <li><strong>Runtime:</strong> ${movie.Runtime}</li>
+      <li><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+      <li><strong>IMDB Votes:</strong> ${movie.imdbVotes}</li>
+      <li><strong>Actors:</strong> ${movie.Actors}</li>
+      <li><strong>Director:</strong> ${movie.Director}</li>
+    `;
+
+    //we gotta refresh because its a list view
+    $('#movieDetails').html(movieDetails).listview('refresh');
   });
 }
